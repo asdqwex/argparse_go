835f563fd5dd985af04ba9f0142f6ae6ca11c257
asdqwex <matthew.ellsworth@matthewellsworth.com>
2016-05-09 20:05:58 -0600

    wired up to use argpare lib

--- argparse.go
+++ argparse.go
@@ -1,16 +1,19 @@
-package main
+package argparse
 
 import (
     "fmt"
     "strings"
-    "os"
 )
 
-func main() {
-  m := make(map[string]string)
-  s := os.Args
+func Parse(s []string) (m map[string]string)  {
+  m = make(map[string]string)
+  pass := 0
   s = append(s[:0], s[1:]...)
   for i := range s {
+    if pass == 1 {
+      pass = 0
+      continue
+    }
     if strings.HasPrefix(s[i], "-") {
       s[i] = s[i][1:]
       if strings.Contains(s[i], "=") {
@@ -21,11 +24,20 @@ func main() {
       i2 := i + 1
       next := s[i2]
       m[s[i]] = next
+      pass = 1
     } else {
       m[s[i]] = "action"
     }
   }
-  for key, value := range m {
-    fmt.Println("Key:", key, "Value:", value)
+  action_counter := 0
+  for _ , v := range m {
+    if v == "action" {
+      action_counter++
+    }
   }
+  if action_counter > 1 {
+    fmt.Println()
+    panic(fmt.Sprintf("%v", "there were more than on actions called"))
+  }
+  return m
 }
